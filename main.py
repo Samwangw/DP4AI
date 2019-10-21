a11c7046a731a44bed3df29d22ae2e3526cde446
Wei <50691976+Samwangw@users.noreply.github.com>
2019-10-21 18:57:44 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from models import thresholdout
+from models import DDSM
+
+
+def main():
+    th = thresholdout.Thresholdout()
+    th.run()
+    # DDSM.ex1()
+    # DDSM.ex2()
+
+
+if __name__ == "__main__":
+    main()
